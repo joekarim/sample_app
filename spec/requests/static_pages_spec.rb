4800e38e3cb1c6e943dc0f448f4d8e96fd7e2ab7
Joe Karim <j.o.karim@gmail.com>
2013-11-19 21:28:47 +0300

    Added a Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,4 +36,15 @@ describe "Static pages" do
                           :text => "Ruby on Rails Test App | About Us")
     end
   end
+  describe "Contact page" do
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                          :text => "Ruby on Rails Test App | Contact Us")
+    end
+  end
 end
